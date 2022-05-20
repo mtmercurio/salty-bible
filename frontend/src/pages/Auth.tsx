e5de8754d185ef898271880763ea8bb704fae7d4
Alex Dykstra <alex@alexdykstra.com>
2022-05-20 12:03:16 -0400

    Setup for local development

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,58 +1,56 @@
-import React, {useEffect, useState} from 'react';
-import {getUsers, User} from '../data/users';
-import {
-    IonButton,
-  IonContent,
-  IonList,
-  IonPage,
-} from '@ionic/react';
-import { useHistory } from 'react-router';
+import React, { useEffect, useState } from "react";
+import { getUsers, User } from "../data/users";
+import { IonButton, IonContent, IonList, IonPage } from "@ionic/react";
+import { useHistory } from "react-router";
 
 const Auth: React.FC = () => {
-
   const [authUrl, setAuthUrl] = useState("");
   const history = useHistory();
 
   useEffect(() => {
-      /**
-   * Construct URL for the callback route.
-   */
-  const url = new URL('https://salty.yas.family/api/google/callback')
+    /**
+     * Construct URL for the callback route.
+     */
+    const url = new URL(
+      process.env.REACT_APP_SERVICE_BASE_URL + "/google/callback"
+    );
 
-  /**
-   * Add the query provided by google.
-   */
-  url.search = window.location.search
+    /**
+     * Add the query provided by google.
+     */
+    url.search = window.location.search;
 
-  /**
-   * Send the final request. You can use Axios if you want.
-   */
-  const response = fetch(url.toString(), {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    },
-    credentials: 'include'
-  }).then(data => data.json()).then(resp => {
-    fetch('https://salty.yas.family/api/authcheck', { 
-      method: 'get', 
-      headers: new Headers({
-          'Authorization': 'Bearer ' + resp.token, 
-      })
-  }).then(resp => resp.json()).then((data) => {
-    if (data.isAuthenticated) {
-      localStorage.setItem('salty_token', resp.token);
-      history.push('/login')
-    }
-  })
-  })
-    
+    /**
+     * Send the final request. You can use Axios if you want.
+     */
+    const response = fetch(url.toString(), {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+      credentials: "include",
+    })
+      .then((data) => data.json())
+      .then((resp) => {
+        fetch(process.env.REACT_APP_SERVICE_BASE_URL + "/authcheck", {
+          method: "get",
+          headers: new Headers({
+            Authorization: "Bearer " + resp.token,
+          }),
+        })
+          .then((resp) => resp.json())
+          .then((data) => {
+            if (data.isAuthenticated) {
+              localStorage.setItem("salty_token", resp.token);
+              history.push("/login");
+            }
+          });
+      });
   }, []);
 
   return (
     <IonPage id="auth-page">
-      <IonContent fullscreen>
-      </IonContent>
+      <IonContent fullscreen></IonContent>
     </IonPage>
   );
 };
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,74 +1,77 @@
-import React, {useEffect, useState} from 'react';
-import {getUsers, User} from '../data/users';
-import {
-    IonButton,
-  IonContent,
-  IonList,
-  IonPage,
-} from '@ionic/react';
+import React, { useEffect, useState } from "react";
+import { getUsers, User } from "../data/users";
+import { IonButton, IonContent, IonList, IonPage } from "@ionic/react";
 import UserListItem from "../components/UserListItem";
 
 const Login: React.FC = () => {
-
   const [users, setUsers] = useState<User[]>([]);
   const [authUrl, setAuthUrl] = useState("");
 
-  const [userContent, setUserContent] = useState('');
-  const [userPlanContent, setUserPlanContent] = useState('');
+  const [userContent, setUserContent] = useState("");
+  const [userPlanContent, setUserPlanContent] = useState("");
 
   useEffect(() => {
-    fetch('https://salty.yas.family/api/google/redirect').then(response => response.json())
-    .then(data => {
+    fetch(process.env.REACT_APP_SERVICE_BASE_URL + "/google/redirect")
+      .then((response) => response.json())
+      .then((data) => {
         setAuthUrl(data.redirectUrl);
-    })
-
-
+      });
   }, []);
 
   const getHeaders = () => {
     let headers;
-    if (localStorage.getItem('salty_token')) {
+    if (localStorage.getItem("salty_token")) {
       headers = new Headers({
-        'Authorization': 'Bearer ' + localStorage.getItem('salty_token')
-      })
+        Authorization: "Bearer " + localStorage.getItem("salty_token"),
+      });
     }
     return headers;
-  }
+  };
 
   const testClick = () => {
-    fetch('https://salty.yas.family/api/authcheck', { 
-    method: 'get', 
-    headers: getHeaders()
-}).then((response) => response.json()).then(d=>console.log(d))
-  }
+    fetch(process.env.REACT_APP_SERVICE_BASE_URL + "/authcheck", {
+      method: "get",
+      headers: getHeaders(),
+    })
+      .then((response) => response.json())
+      .then((d) => console.log(d));
+  };
 
   const logoutClick = () => {
-      fetch('https://salty.yas.family/api/logout', {
-        method: 'post', 
-        headers: getHeaders()
-    }).then((response) => response.json()).then(d=>console.log(d))
-  }
+    fetch(process.env.REACT_APP_SERVICE_BASE_URL + "/logout", {
+      method: "post",
+      headers: getHeaders(),
+    })
+      .then((response) => response.json())
+      .then((d) => console.log(d));
+  };
 
   const getUserClick = () => {
-    fetch('https://salty.yas.family/api/user', {
-      method: 'get', 
-      headers: getHeaders()
-  }).then((response) => response.json()).then(d=>setUserContent(JSON.stringify(d, null, 2)))
-}
+    fetch(process.env.REACT_APP_SERVICE_BASE_URL + "/user", {
+      method: "get",
+      headers: getHeaders(),
+    })
+      .then((response) => response.json())
+      .then((d) => setUserContent(JSON.stringify(d, null, 2)));
+  };
 
   const getUserPlanClick = () => {
-    fetch('https://salty.yas.family/api/user/plan', {
-      method: 'get', 
-      headers: getHeaders()
-  }).then((response) => response.json()).then(d=>setUserPlanContent(JSON.stringify(d, null, 2)))
-}
+    fetch(process.env.REACT_APP_SERVICE_BASE_URL + "/user/plan", {
+      method: "get",
+      headers: getHeaders(),
+    })
+      .then((response) => response.json())
+      .then((d) => setUserPlanContent(JSON.stringify(d, null, 2)));
+  };
 
   const assignUserTestPlan = () => {
-    fetch('https://salty.yas.family/api/user/plan/assign', {
-      method: 'get', 
-      headers: getHeaders()
-  }).then((response) => response.json()).then(d=>console.log(d))
-}
+    fetch(process.env.REACT_APP_SERVICE_BASE_URL + "/user/plan/assign", {
+      method: "get",
+      headers: getHeaders(),
+    })
+      .then((response) => response.json())
+      .then((d) => console.log(d));
+  };
 
   return (
     <IonPage id="login-page">
@@ -76,17 +79,20 @@ const Login: React.FC = () => {
         <IonButton href={authUrl}>Login</IonButton>
         <IonButton onClick={testClick}>Test Auth</IonButton>
         <IonButton onClick={logoutClick}>Logout</IonButton>
-        
-        
+
         <div>
           <IonButton onClick={getUserClick}>Get User</IonButton>
-          <code><pre>{userContent}</pre></code>
+          <code>
+            <pre>{userContent}</pre>
+          </code>
         </div>
 
         <div>
           <IonButton onClick={getUserPlanClick}>Get User's Plan</IonButton>
           <IonButton onClick={assignUserTestPlan}>Assign Test Plan</IonButton>
-          <code><pre>{userPlanContent}</pre></code>
+          <code>
+            <pre>{userPlanContent}</pre>
+          </code>
         </div>
       </IonContent>
     </IonPage>
