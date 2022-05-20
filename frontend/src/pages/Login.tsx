cc5ae1e067e767f83b234aa34df0f696a67e312c
Alex Dykstra <alex@alexdykstra.com>
2022-05-19 21:06:51 -0400

    ready for work frontend

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -13,6 +13,9 @@ const Login: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [authUrl, setAuthUrl] = useState("");
 
+  const [userContent, setUserContent] = useState('');
+  const [userPlanContent, setUserPlanContent] = useState('');
+
   useEffect(() => {
     fetch('https://salty.yas.family/api/google/redirect').then(response => response.json())
     .then(data => {
@@ -46,12 +49,45 @@ const Login: React.FC = () => {
     }).then((response) => response.json()).then(d=>console.log(d))
   }
 
+  const getUserClick = () => {
+    fetch('https://salty.yas.family/api/user', {
+      method: 'get', 
+      headers: getHeaders()
+  }).then((response) => response.json()).then(d=>setUserContent(JSON.stringify(d, null, 2)))
+}
+
+  const getUserPlanClick = () => {
+    fetch('https://salty.yas.family/api/user/plan', {
+      method: 'get', 
+      headers: getHeaders()
+  }).then((response) => response.json()).then(d=>setUserPlanContent(JSON.stringify(d, null, 2)))
+}
+
+  const assignUserTestPlan = () => {
+    fetch('https://salty.yas.family/api/user/plan/assign', {
+      method: 'get', 
+      headers: getHeaders()
+  }).then((response) => response.json()).then(d=>console.log(d))
+}
+
   return (
     <IonPage id="login-page">
       <IonContent fullscreen>
         <IonButton href={authUrl}>Login</IonButton>
         <IonButton onClick={testClick}>Test Auth</IonButton>
         <IonButton onClick={logoutClick}>Logout</IonButton>
+        
+        
+        <div>
+          <IonButton onClick={getUserClick}>Get User</IonButton>
+          <code><pre>{userContent}</pre></code>
+        </div>
+
+        <div>
+          <IonButton onClick={getUserPlanClick}>Get User's Plan</IonButton>
+          <IonButton onClick={assignUserTestPlan}>Assign Test Plan</IonButton>
+          <code><pre>{userPlanContent}</pre></code>
+        </div>
       </IonContent>
     </IonPage>
   );
